50e361d881725e4b0998996e53d8cd42704d9f7c
LucianoBWille <00lucianobwille00@gmail.com>
2020-03-21 15:24:07 -0300

    Exercicio 001 com todas as funcinalidades implementadas

--- Exercicio001.py
+++ Exercicio001.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+from __future__ import division
+import argparse
 import numpy as np
 import cv2
 #from matplotlib import pyplot as plt
@@ -18,17 +21,62 @@ def splitter(img, name):
     cv2.imwrite('imagens//' + name + '-Channel 3.jpg', ch3)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+def histogramer(src, name):
+    bgr_planes = cv2.split(src)
+    histSize = 256
+    histRange = (0, 256)  # the upper boundary is exclusive
+    accumulate = False
+    b_hist = cv2.calcHist(bgr_planes, [0], None, [histSize], histRange, accumulate=accumulate)
+    g_hist = cv2.calcHist(bgr_planes, [1], None, [histSize], histRange, accumulate=accumulate)
+    r_hist = cv2.calcHist(bgr_planes, [2], None, [histSize], histRange, accumulate=accumulate)
+    hist_w = 256
+    hist_h = 256
+    bin_w = int(round(hist_w / histSize))
+    maior = max([max(b_hist), max(g_hist), max(r_hist)])
+    histImage = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
+
+    for i in range(1, histSize):
+        y1 = hist_h - 1 - int(((hist_h - 2) / maior) * (b_hist[i - 1]))
+        y2 = hist_h - 1 - int(((hist_h - 2) / maior) * (b_hist[i]))
+        cv2.line(histImage, (bin_w * (i - 1), y1), (bin_w * (i), y2), (255, 0, 0), thickness=bin_w)
+        y1 = hist_h - 1 - int(((hist_h - 2) / maior) * (g_hist[i - 1]))
+        y2 = hist_h - 1 - int(((hist_h - 2) / maior) * (g_hist[i]))
+        cv2.line(histImage, (bin_w * (i - 1), y1), (bin_w * (i), y2), (0, 255, 0), thickness=bin_w)
+        y1 = hist_h - 1 - int(((hist_h - 2) / maior) * (r_hist[i - 1]))
+        y2 = hist_h - 1 - int(((hist_h - 2) / maior) * (r_hist[i]))
+        cv2.line(histImage, (bin_w * (i - 1), y1), (bin_w * (i), y2), (0, 0, 255), thickness=bin_w)
+    cv2.imshow(name + '-Histograma', histImage)
+    cv2.imwrite('imagens//' + name + '-Histograma.jpg', histImage)
+def histogramer1channel(src, name):
+    histSize = 256
+    histRange = (0, 256)  # the upper boundary is exclusive
+    accumulate = False
+    hist = cv2.calcHist(src, [0], None, [histSize], histRange, accumulate=accumulate)
+    hist_w = 256
+    hist_h = 256
+    bin_w = int(round(hist_w / histSize))
+    maior = max(hist)
+    histImage = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
+
+    for i in range(1, histSize):
+        y1 = hist_h - 1 - int(((hist_h - 2) / maior) * (hist[i - 1]))
+        y2 = hist_h - 1 - int(((hist_h - 2) / maior) * (hist[i]))
+        cv2.line(histImage, (bin_w * (i - 1), y1), (bin_w * (i), y2), (255, 255, 255), thickness=bin_w)
+    cv2.imshow(name + '-Histograma', histImage)
+    cv2.imwrite('imagens//' + name + '-Histograma.jpg', histImage)
 op = 1
 initFlag = True
 while op != 0:
     op = int(input('''     =================
-     | 0 - Sair      |
-     | 1 - HSV       |
-     | 2 - LAB       |
-     | 3 - LUV       |
-     | 4 - XYZ       |
-     | 5 - YUV       |
-     | 6 - YCrCb     |
+     | 0 - Sair       |
+     | 1 - HSV        |
+     | 2 - HLS        |
+     | 3 - CIE LAB    |
+     | 4 - CIE LUV    |
+     | 5 - XYZ        |
+     | 6 - YCrCb      |
+     | 7 - YUV        |
+     | 8 - GRAY       |
      =================
      Opção: '''))
     if op == 0:
@@ -39,44 +87,66 @@ while op != 0:
     image = cv2.imread(nomeArq + '.jpg')
     nome = nome.capitalize()
     cv2.imshow(nome, image)
+    histogramer(image, nome)
     os.system('clear')
     if op == 1:  # HSV
         hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
         nome = nome + ' HSV'
         cv2.imshow(nome, hsv)
+        histogramer(hsv, nome)
         splitter(hsv, nome)
         del hsv
-    elif op == 2:  # Lab
+    if op == 2:  # HLS
+        hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+        nome = nome + ' HLS'
+        cv2.imshow(nome, hls)
+        histogramer(hls, nome)
+        splitter(hls, nome)
+        del hsv
+    elif op == 3:  # Lab
         lab = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
         nome = nome + ' LAB'
         cv2.imshow(nome, lab)
+        histogramer(lab, nome)
         splitter(lab, nome)
         del lab
-    elif op == 3:  # LUV
+    elif op == 4:  # LUV
         luv = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
         nome = nome + ' LUV'
         cv2.imshow(nome, luv)
+        histogramer(luv, nome)
         splitter(luv, nome)
         del luv
-    elif op == 4:  # XYZ
+    elif op == 5:  # XYZ
         xyz = cv2.cvtColor(image, cv2.COLOR_RGB2XYZ)
         nome = nome + ' XYZ'
         cv2.imshow(nome, xyz)
+        histogramer(xyz, nome)
         splitter(xyz, nome)
         del xyz
-    elif op == 5:  # YUV
-        yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-        nome = nome + ' YUV'
-        cv2.imshow(nome, yuv)
-        splitter(yuv, nome)
-        del yuv
     elif op == 6:  # YCrCb
         YCrCb = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
         nome = nome + ' YCrCb'
-        cv2.imshow(nome, YCrCb)
+        cv2.imshow(nome, )
+        histogramer(YCrCb, nome)
         splitter(YCrCb, nome)
         del YCrCb
+    elif op == 7:  # YUV
+        yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        nome = nome + ' YUV'
+        cv2.imshow(nome, yuv)
+        histogramer(yuv, nome)
+        splitter(yuv, nome)
+        del yuv
+    elif op == 8:  # GRAY
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        nome = nome + ' GRAY'
+        histogramer1channel(gray, nome)
+        cv2.imshow(nome, gray)
+        cv2.imwrite('imagens//' + nome + '.jpg', gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        del gray
     else:
         print('Opção Inválida')
         cv2.destroyAllWindows()
-        
--- _Testes_.py
+++ _Testes_.py
@@ -1,30 +1,77 @@
+from __future__ import print_function
+from __future__ import division
+import argparse
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
 
-class Conversor():
-    def __init__(self, localArq):
-        self.local_arquivo = localArq
-        self.imgOriginal = cv.imread(localArq, cv.IMREAD_COLOR)
-        arqtxt = str(localArq).split('/')
-        arqtxt = arqtxt[-1]
-        nome = arqtxt.split('.')[0]
-        self.local = '/'.join(arqtxt[:-1])
+
+def converter(img, formatoSaida, formatoEntrada='RGB'):
+    comando = eval(f'cv.COLOR_{formatoEntrada}2{formatoSaida}')
+    return cv.cvtColor(img, comando)
+
+
+class Image():
+    def __init__(self, nome, tipo, img):
         self.nome = nome
-        self.extencao = arqtxt.split('.')[1]
-    def exibeImagens(self):
-        cv.imshow('Imagem', self.imgOriginal)
-    def bgr2hsv(self):
-        self.hsv = cv.cvtColor(self.imgOriginal, cv.COLOR_BGR2HSV)
-    def bgr2lab(self):
-        self.lab = cv.cvtColor(self.imgOriginal, cv.COLOR_BGR2LAB)
-    def bgr2luv(self):
-        self.luv = cv.cvtColor(self.imgOriginal, cv.COLOR_BGR2LUV)
+        self.tipo = tipo
+        self.imagem = img
 
-conv = Conversor('imagens/Lenna XYZ.jpg')
-print(conv.local)
-conv.exibeImagens()
-conv.bgr2hsv()
-cv.imshow('HSV', conv.hsv)
+
+
+original = Image('Lenna', 'RGB', cv.imread('Lenna.jpg'))
+'''
+xyz = Image('Lenna', 'XYZ', converter(original.imagem, 'XYZ', formatoEntrada=original.tipo))
+cv.imshow(f'{original.nome} {original.tipo}', original.imagem)
+cv.imshow(f'{xyz.nome} {xyz.tipo}', xyz.imagem)
 cv.waitKey(0)
 cv.destroyAllWindows()
+hist = cv.calcHist([original.imagem], [0], None, [256], [0, 256])
+print(hist)
+cv.imshow('histograma', hist)
+cv.waitKey(0)
+cv.destroyAllWindows()
+'''
+'''
+parser = argparse.ArgumentParser(description='Code for Histogram Calculation tutorial.')
+parser.add_argument('--input', help='Path to input image.', default='lena.jpg')
+args = parser.parse_args()
+'''
+#src = cv.imread(cv.samples.findFile(args.input))
+src = original.imagem
+if src is None:
+    print('Could not open or find the image:', args.input)
+    exit(0)
+bgr_planes = cv.split(src)
+histSize = 256
+histRange = (0, 256)  # the upper boundary is exclusive
+accumulate = False
+b_hist = cv.calcHist(bgr_planes, [0], None, [histSize], histRange, accumulate=accumulate)
+g_hist = cv.calcHist(bgr_planes, [1], None, [histSize], histRange, accumulate=accumulate)
+r_hist = cv.calcHist(bgr_planes, [2], None, [histSize], histRange, accumulate=accumulate)
+hist_w = 512
+hist_h = 512
+bin_w = int(round( hist_w/histSize ))
+maior = max([max(b_hist), max(g_hist), max(r_hist)])
+histImage = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
+
+cv.normalize(b_hist, b_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+cv.normalize(g_hist, g_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+cv.normalize(r_hist, r_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+
+for i in range(1, histSize):
+    y1 = hist_h - 1 - int(((hist_h-2)/maior)*(b_hist[i-1]))
+    y2 = hist_h - 1 - int(((hist_h-2)/maior)*(b_hist[i]))
+    cv.line(histImage, ( bin_w*(i-1), y1 ), ( bin_w*(i), y2 ), ( 255, 0, 0), thickness=2)
+    y1 = hist_h - 1 - int(((hist_h-2)/maior)*(g_hist[i-1]))
+    y2 = hist_h - 1 - int(((hist_h-2)/maior)*(g_hist[i]))
+    cv.line(histImage, ( bin_w*(i-1), y1 ), ( bin_w*(i), y2 ), ( 0, 255, 0), thickness=2)
+    y1 = hist_h - 1 - int(((hist_h-2)/maior)*(r_hist[i-1]))
+    y2 = hist_h - 1 - int(((hist_h-2)/maior)*(r_hist[i]))
+    cv.line(histImage, ( bin_w*(i-1), y1), ( bin_w*(i), y2 ), ( 0, 0, 255), thickness=2)
+cv.imshow('Source image', src)
+cv.imshow('calcHist Demo', histImage)
+image = cv.merge((b_hist, g_hist, r_hist))
+cv.imshow('Merged', image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
